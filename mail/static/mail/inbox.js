e260fea247e24f5e0f61cffe1fc7bb007dcf2415
Gabriel Akara <kaielakara@gmail.com>
2024-03-05 14:20:37 +0100

    Fetch API Implementation
    The Implementation of getting data from the serverside and the Implementation of POST data to the server side which while uploading data using the POST converts the form data into JSON. While the fetch for getting data get JSON file whichyou can access.
    
    We also created a new element while trying to show the output. We did this using JavaScript functions such as document.createElement() and document.append()'

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
+  document.querySelector('#compose-form').addEventListener('submit', submit_mail);
 
   // By default, load the inbox
   load_mailbox('inbox');
@@ -30,4 +31,43 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+  fetch(`/emails/${mailbox}`)
+  .then(response => response.json())
+  .then(emails => {
+
+    for (let index = 0; index < emails.length; index++) {
+      let email = emails[index];
+      let email_div = document.createElement("div");
+      document.querySelector('#emails-view').append(email_div);
+      let email_user = document.createElement('p');
+      email_user.innerHTML = email.sender;
+      email_div.append(email_user);
+      let email_subject = document.createElement('p');
+      email_subject.innerHTML = email.subject;
+      email_div.append(email_subject);
+      let email_time = document.createElement('p');
+      email_time.innerHTML = email.timestamp;
+      email_div.append(email_time);
+      email_time.className = "email_timestamp"
+      console.log(emails);
+    }
+    
+  })
+}
+
+function submit_mail() {
+
+  fetch("emails/", {
+    method: "POST",
+    body: JSON.stringify({
+      recipients: document.querySelector('#compose-recipients').value,
+      subject: document.querySelector("#compose-subject").value,
+      body: document.querySelector("#compose-body").value
+    })
+  })
+
+  .then(response => response.json())
+  .then(result => {
+    console.log(result);
+  })
 }
